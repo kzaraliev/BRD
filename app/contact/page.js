741eb15c5073d36c878e166fddf32541c12bad84
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-03-05 15:37:43 +0200

    Connect form with wordpress

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,79 @@
-import { BuildingOffice2Icon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+"use client";
+
+import {
+  BuildingOffice2Icon,
+  EnvelopeIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+import Swal from "sweetalert2";
+import { useState } from "react";
+
+const URL_FORM =
+  "https://brd.devclick.net/wp-json/contact-form-7/v1/contact-forms/43/feedback";
 
 export default function Contact() {
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const subject = e.target.subject.value;
+    const phoneNumber = e.target["phone-number"].value;
+    const message = e.target.message.value;
+
+    formData.append("_wpcf7_unit_tag", "43");
+    formData.append("your-name", name);
+    formData.append("your-email", email);
+    formData.append("your-subject", subject);
+    formData.append("your-tel", phoneNumber);
+    formData.append("your-message", message);
+
+    console.log(formData);
+
+    const reqOptions = {
+      method: "POST",
+      body: formData,
+    };
+
+    try {
+      const req = await fetch(URL_FORM, reqOptions);
+      const response = await req.json();
+
+      if (response.status === "validation_failed") {
+        let fieldErrors = {};
+        response.invalid_fields.forEach((field) => {
+          fieldErrors[field.field] = field.message;
+        });
+        setErrors(fieldErrors);
+      } else if (response.status === "mail_sent") {
+        Swal.fire({
+          timer: 4000,
+          title: "Успешно изпратено съобщение!",
+          text: "Очаквайте отговор скоро : )",
+          icon: "success",
+        });
+        setErrors({});
+        e.target.reset();
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Неочаквана грешка...",
+          text: "Нещо се случи! Опитайте отново по-късно",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Неуспешно изпращане!",
+        text: "Проверете връзката с интернет и опитайте отново.",
+      });
+    }
+  };
+
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -27,21 +100,30 @@ export default function Contact() {
                 <svg x="100%" y={-1} className="overflow-visible fill-gray-50">
                   <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
                 </svg>
-                <rect fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" width="100%" height="100%" strokeWidth={0} />
+                <rect
+                  fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
+                  width="100%"
+                  height="100%"
+                  strokeWidth={0}
+                />
               </svg>
             </div>
             <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
               Get in touch
             </h2>
             <p className="mt-6 text-lg/8 text-gray-600">
-              Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt
-              integer elementum id sem. Arcu sed malesuada et magna.
+              Proin volutpat consequat porttitor cras nullam gravida at. Orci
+              molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
+              Arcu sed malesuada et magna.
             </p>
             <dl className="mt-10 space-y-4 text-base/7 text-gray-600">
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Address</span>
-                  <BuildingOffice2Icon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                  <BuildingOffice2Icon
+                    aria-hidden="true"
+                    className="h-7 w-6 text-gray-400"
+                  />
                 </dt>
                 <dd>
                   545 Mavis Island
@@ -52,10 +134,16 @@ export default function Contact() {
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Telephone</span>
-                  <PhoneIcon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                  <PhoneIcon
+                    aria-hidden="true"
+                    className="h-7 w-6 text-gray-400"
+                  />
                 </dt>
                 <dd>
-                  <a href="tel:+1 (555) 234-5678" className="hover:text-gray-900">
+                  <a
+                    href="tel:+1 (555) 234-5678"
+                    className="hover:text-gray-900"
+                  >
                     +1 (555) 234-5678
                   </a>
                 </dd>
@@ -63,10 +151,16 @@ export default function Contact() {
               <div className="flex gap-x-4">
                 <dt className="flex-none">
                   <span className="sr-only">Email</span>
-                  <EnvelopeIcon aria-hidden="true" className="h-7 w-6 text-gray-400" />
+                  <EnvelopeIcon
+                    aria-hidden="true"
+                    className="h-7 w-6 text-gray-400"
+                  />
                 </dt>
                 <dd>
-                  <a href="mailto:hello@example.com" className="hover:text-gray-900">
+                  <a
+                    href="mailto:hello@example.com"
+                    className="hover:text-gray-900"
+                  >
                     hello@example.com
                   </a>
                 </dd>
@@ -74,40 +168,40 @@ export default function Contact() {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" className="px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48">
+        <form
+          onSubmit={handleSubmit}
+          className="px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48"
+        >
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div>
-                <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
-                  First name
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="name"
+                  className="block text-sm/6 font-semibold text-gray-900"
+                >
+                  Име*
                 </label>
                 <div className="mt-2.5">
                   <input
-                    id="first-name"
-                    name="first-name"
+                    id="name"
+                    name="name"
                     type="text"
-                    autoComplete="given-name"
+                    autoComplete="name"
                     className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
-              </div>
-              <div>
-                <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
-                  Last name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    id="last-name"
-                    name="last-name"
-                    type="text"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                  />
-                </div>
+                {errors["your-name"] && (
+                  <p className="text-red-600 text-sm mt-1">
+                    {errors["your-name"]}
+                  </p>
+                )}
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
-                  Email
+                <label
+                  htmlFor="email"
+                  className="block text-sm/6 font-semibold text-gray-900"
+                >
+                  Имейл*
                 </label>
                 <div className="mt-2.5">
                   <input
@@ -118,10 +212,18 @@ export default function Contact() {
                     className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
+                {errors["your-email"] && (
+                  <p className="text-red-600 text-sm mt-1">
+                    {errors["your-email"]}
+                  </p>
+                )}
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
-                  Phone number
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm/6 font-semibold text-gray-900"
+                >
+                  Телефон*
                 </label>
                 <div className="mt-2.5">
                   <input
@@ -132,10 +234,39 @@ export default function Contact() {
                     className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
+                {errors["your-tel"] && (
+                  <p className="text-red-600 text-sm mt-1">
+                    {errors["your-tel"]}
+                  </p>
+                )}
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
-                  Message
+                <label
+                  htmlFor="subject"
+                  className="block text-sm/6 font-semibold text-gray-900"
+                >
+                  Тема*
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="subject"
+                    name="subject"
+                    type="text"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  />
+                </div>
+                {errors["your-subject"] && (
+                  <p className="text-red-600 text-sm mt-1">
+                    {errors["your-subject"]}
+                  </p>
+                )}
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm/6 font-semibold text-gray-900"
+                >
+                  Съобщение
                 </label>
                 <div className="mt-2.5">
                   <textarea
@@ -143,7 +274,7 @@ export default function Contact() {
                     name="message"
                     rows={4}
                     className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                    defaultValue={''}
+                    defaultValue={""}
                   />
                 </div>
               </div>
@@ -153,12 +284,12 @@ export default function Contact() {
                 type="submit"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Send message
+                Изпрати запитване
               </button>
             </div>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
