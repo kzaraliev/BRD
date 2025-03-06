c9082a96c40b0fa0e8078537544390d5b0c1a6f6
7onc1 <anton.demirev84@gmail.com>
2025-03-06 11:22:34 +0200

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -90,7 +90,7 @@ export default function Contact() {
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div className="relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:px-8 lg:py-48">
+        <div className="relative px-6 pt-24 pb-20 sm:pt-24 lg:static lg:px-8 lg:py-24">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
             <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
               <svg
@@ -196,7 +196,7 @@ export default function Contact() {
         </div>
         <form
           onSubmit={handleSubmit}
-          className="px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48"
+          className="px-6 pt-20 pb-24 sm:pb-24 lg:px-8 lg:py-24"
         >
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
