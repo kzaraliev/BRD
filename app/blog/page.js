bc4627a9f2778f3ee7f5b06dfebab84471eba3c6
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-03-09 21:27:59 +0200

    fix color for the spinner

--- app/blog/page.js
+++ app/blog/page.js
@@ -45,7 +45,7 @@ export default function Blog() {
         {/* Loader */}
         {loading && (
           <div className="flex justify-center mt-10">
-            <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
+            <div className="w-12 h-12 border-4 border-gray-300 border-t-[#95161C] rounded-full animate-spin"></div>
           </div>
         )}
 
