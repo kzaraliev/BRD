390a6f88e2b6969d509debd1c70a49b768325781
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-03-12 14:00:43 +0200

    Get only necessary fields for post

--- services/posts.js
+++ services/posts.js
@@ -1,20 +1,12 @@
 import { fetchAPI } from "./api";
 
-// /**
-//  * Get all posts
-//  * @returns {Promise<Array>} - List of posts
-//  */
-// export const getPosts = async () => {
-//     return await fetchAPI("posts");
-// };
-
 /**
  * Get single post by slug
  * @param {string} slug - Post slug
  * @returns {Promise<Object|null>} - Post data
  */
 export const getPostBySlug = async (slug) => {
-    return await fetchAPI(`posts?slug=${slug}`);
+    return await fetchAPI(`posts?slug=${slug}&_fields=id,slug,yoast_head_json,date,title,content`);
 };
 
 /**
