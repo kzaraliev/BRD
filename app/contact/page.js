7a0ca15a9d588c0caaa8269c5f5637511420c97c
7onc1 <anton.demirev84@gmail.com>
2025-03-29 13:50:01 +0200

    Test fix LCP

--- app/contact/page.js
+++ app/contact/page.js
@@ -21,7 +21,7 @@ export default async function ContactPage() {
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div className="relative px-6 pt-24 pb-20 sm:pt-24 lg:static lg:px-8 lg:py-24">
+        <div className="px-6 pt-24 pb-20 sm:pt-24 lg:static lg:px-8 lg:py-24">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
             <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
               <svg
--- app/layout.js
+++ app/layout.js
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="bg">
       <head>
-        {/* <CriticalCSS />
+        <CriticalCSS />
         <link
           rel="preconnect"
           href="https://brd.devclick.net"
@@ -81,7 +81,7 @@ export default function RootLayout({ children }) {
           type="image/webp"
           fetchpriority="high"
           importance="high"
-        /> */}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={roboto.className}>
